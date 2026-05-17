37d0cf9bc0cc85e5595711d55bcb84ba136aef9c
Angel <angel.peraza@peaku.io>
2026-05-17 05:53:17 -0500

    Productos crud funcionando completo

--- src/schemas/productos.schema.js
+++ src/schemas/productos.schema.js
@@ -2,13 +2,13 @@ const productoSchema = {
     type: 'object',
     required: ['nombre', 'precio'],
     properties: {
-        id: { type: 'integer', description: 'ID autogenerado' },
-        nombre: { type: 'string' },
+        //id: { type: 'integer', description: 'ID autogenerado' },
+        nombre: { type: 'string', example:'papa' },
         descripcion: { type: 'string' },
-        precio: { type: 'number' },
-        stock: { type: 'integer' },
+        precio: { type: 'number', example: 1000 },
+        stock: { type: 'integer', example: 1 },
         imagen_url: { type: 'string' },
-        categoria: { type: 'string' }
+        categoria: { type: 'string', example: 'Frutas y verduras'}
     }
 };
 
